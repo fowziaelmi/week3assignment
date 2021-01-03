344c41cb59bf9610b8d52505c4e078e404ff20e0
Faduma Elmi <fowziiae@gmail.com>
2021-01-03 16:18:20 -0600

    initial commit

---
+++ scripts/1-array-practice.js
@@ -0,0 +1,81 @@
+console.log('****** Array Practice *******');
+
+// 1. Creating arrays
+console.log('--- 1. Practice creating arrays ---');
+
+// Example: Array of animals
+let animalArray = ['fish', 'cat', 'bird', 'dog'];
+console.log('Animals are: ', animalArray);
+
+// TODO: Make an array with some favorite foods & then log it
+// to the console with a message similar to the example above
+let favFoods= ['rice', 'pasta', 'chicken', 'tacos', 'fries' ];
+console.log('My favorite foods are: ', favFoods);
+
+// 2. Array.length - tells you how many items are in the array
+console.log('--- 2. Length of an array ---');
+
+// Example: How many animals are in the array?
+console.log('Number of animals:', animalArray.length);
+
+// TODO: Log to the console the number of foods in your array
+console.log('Number of foods:', favFoods.length);
+
+// 3. Accessing array items
+console.log('--- 3. Accessing items in an array ---');
+
+// Example: Log the first animal from the array using it's array index
+console.log('First animal is:', animalArray[0])
+
+// TODO: Log the second animal in the array
+console.log('The second animal is:', animalArray[1]);
+
+// TODO: Log the last animal in the array using it's array index
+console.log('The last animal is:', animalArray[3]);
+
+// TODO - Stretch:
+// Do this by using the array length instead of the exact index number
+console.log('The last animal is:', animalArray[animalArray.length - 1]);
+
+
+// 4. Adding & Removing Array Items
+console.log('--- 4. Adding and removing array items ---');
+
+// Example: Add an animal to the end using Array.push
+animalArray.push('penguin');
+console.log('Added an animal to end,', animalArray);
+
+// TODO: Add a new food at the end of your array & log the array
+favFoods.push('shrimp');
+console.log('Added a favorite food to the end,', favFoods);
+
+
+// Example: Remove the last animal by using Array.pop
+let removedAnimal = animalArray.pop();
+console.log('Removed the last animal', removedAnimal);
+console.log('The animals are now', animalArray);
+
+// TODO: Remove the food at the end of your array &
+//       log both the food removed and the updated array
+let removedFood = favFoods.pop();
+console.log('Removed the last food', removedFood);
+console.log('Favorite foods are now:',favFoods );
+
+// Example: Add an animal to the beginning using Array.unshift
+animalArray.unshift('walrus');
+console.log(`Added an animal to beginning: ${animalArray}`);
+
+// TODO: Add a food at the beginning of the array & log the array
+favFoods.unshift('crab');
+console.log('Added a favorite food to the beginning:', favFoods);
+
+// Example: Remove the first animal using Array.shift
+removedAnimal = animalArray.shift();
+console.log('Removed the first animal', removedAnimal);
+console.log('The animals are now', animalArray);
+
+// TODO: Remove the food at the beginning of your array &
+//       log both the food removed and the updated array
+removedFood = favFoods.shift();
+console.log('Removed the first food', removedFood);
+console.log('My favorite foods are now:', favFoods);
---
+++ scripts/2-loops-practice.js
@@ -0,0 +1,72 @@
+console.log('****** Loops Practice *******');
+
+
+// 1. 'for' loop
+console.log('---- 1. For loops ----');
+// Example: a for loop to console.log numbers from 0 to 3
+console.log('count from 0 to 3');
+// start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
+for (let i=0; i<4; i++) {
+  console.log(i);
+}
+
+// TODO: Write a for loop to console.log the numbers from 0 to 5
+//   - Which part of the example loop do you need to change to do this?
+console.log('count from 0 to 5');
+for (let i=0; i<6; i++) {
+  console.log(i);
+}
+// TODO: Write a for loop to console.log the numbers from 3 to 5
+//   - Which part of the example loop do you need to change to do this?
+console.log('count from 3 to 5');
+for  (let i=3; i<6; i++) {
+  console.log(i);
+}
+// TODO: Write a for loop to console.log EVEN numbers from 2 to 10
+//   - Which part of the example loop do you need to change to do this?
+console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for (let i=2; i<11; i++){
+  if (i % 2 === 0){
+    console.log(i);
+  }
+}
+// STRETCH: Write a for loop to do a counddown from 5 to 0
+console.log('countdown from 5 to 0');
+for (let i=5; i>-1; i--){
+  console.log(i);
+}
+
+// 2. For of loops
+console.log('---- 2. For Of loop ----');
+let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
+
+// TODO: Write a for of loop to console.log each star in the 'stars' array
+console.log('Some stars:');
+for (let i=0; i<stars.length; i++);{
+console.log('Stars in the array:', stars);
+}
+// 3. While loops
+console.log('---- 3. While loop ----');
+
+// TODO: Write a while loop to console.log each star in the 'stars' array
+console.log('Some stars using while:');
+let i=0;
+while (i<stars.length){
+  console.log('Stars in the array are:', stars);
+  i++;
+}
+// TODO: Write a while loop to console.log the numbers from 0 to 5
+console.log('count from 0 to 5');
+i=0
+while (i<6){
+console.log(i);
+i++;
+}
+// TO
+//DO: Write a while loop to console.log the numbers from 10 to 5
+console.log('count backwards from 10 to 5');
+i=10
+while (i>4){
+  console.log(i);
+  i--;
+}
